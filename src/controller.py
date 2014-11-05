2b56679db5cacbeede9faf83237f60957f0bfbc1
Spencer Schack <scs1818@gmail.com>
2014-11-04 23:24:28 -0800

    added high level state machine to controller class

--- src/controller.py
+++ src/controller.py
@@ -4,18 +4,57 @@ import baxter_interface
 class Controller:
 
 	def __init__(self):
+		self.state = 'initializingRobot'
+		while True:
+			# Calls the method 'state<State>'
+			getattr(self, 'state' + self.state.capitalize())()
+
+	def stateInitializingRobot(self):
 		baxter_interface.RobotEnable().enable()
 		self.initGrippers()
 		self.initCameras()
+		self.initLimbs()
+		self.state = 'locatingBoard'
 
-	def initGrippers():
+	def stateLocatingBoard(self):
+		# Pan head and find the chessboard
+		self.headCamera.open()
+		self.state = 'initializingBoard'
+
+	def stateInitializingBoard(self):
+		# Place the pieces
+		if self.isTurn:
+			self.state = 'pondering'
+		else:
+			self.state = 'waiting'
+
+	def statePondering(self):
+		# Pass piece positions to chess engine and wait for response
+		if self.gameOver:
+			self.state = 'terminating'
+		else:
+			self.state = 'moving'
+
+	def stateWaiting(self):
+		# Wait until player moves.
+		self.state = 'moving'
+
+	def stateMoving(self):
+		# Get coordinates of piece to move, pass to moveit, and command limb.
+		self.state = 'waiting'
+
+	def stateTerminating():
+		# Do whatever ROS needs to do to shutdown
+		pass
+
+	def initGrippers(self):
 		self.leftGripper = baxter_interface.Gripper('left')
 		self.rightGripper = baxter_interface.Gripper('right')
 		# These calibrate calls are blocking.
 		self.leftGripper.calibrate()
 		self.rightGripper.calibrate()
 
-	def initCameras():
+	def initCameras(self):
 		# These are closed by default. You can only have two open at a time.
 		self.headCamera = baxter_interface.CameraController('head_camera')
 		self.leftHandCamera = baxter_interface.CameraController('left_hand_camera')
@@ -24,6 +63,10 @@ class Controller:
 		self.headCamera.resolution = resolution
 		self.leftHandCamera.resolution = resolution
 		self.rightHandCamera.resolution = resolution
+
+	def initLimbs(self):
+		self.leftLimb = baxter_interface.Limb('left')
+		self.rightLimb = baxter_interface.Limb('right')
 
 if __name__ == '__main__':
 	Controller()
